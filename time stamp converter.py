48be218c9dda06648066860cefb4265f2293a653
timeparrakeet <timeparakeet@gmail.com>
2022-01-14 20:10:01 +0000

    Create time stamp converter.py

---
+++ time stamp converter.py
@@ -0,0 +1,19 @@
+from datetime import date
+from datetime import datetime
+while True:
+    t = input("do you want to convert a date to a time stamp press n to do so")
+    if t == "n":
+        d = str(input("enter a date in this order(yyyy-mm-dd):"))
+        user_date = datetime.strptime(d, "%Y-%m-%d")
+        user_timestamp = datetime.timestamp(user_date)
+        print("your time stamp is", user_timestamp)
+        
+    x = input("do you want to convert a time stampt to a date press y to do so")
+    if x == "y":
+        timestamp = int(input("enter a time stamp here:"))
+        result = date.fromtimestamp(timestamp)
+        print(result)
+        exit = input("press q to exit if you wish")
+        if exit == "q":
+            break
+    
